b8793ab056bce9e004ffc3067fc28bb26bd72bdf
JoaoPLS-1 <barrancoij@gmail.com>
2026-02-25 17:45:56 -0300

    Componentização dos botões

---
+++ src/assets/components/buttonCadastrar.tsx
@@ -0,0 +1,7 @@
+export function ButtonCadastrar() {
+    return (
+        <>
+        <button className="border border-zinc-400 px-4 py-1 bg-zinc-800 text-white cursor-pointer" type="submit">Cadastrar</button>
+        </>
+    )
+}
---
+++ src/assets/components/buttonLimpar.tsx
@@ -0,0 +1,7 @@
+export function ButtonLimpar() {
+    return (
+        <>
+        <button className="px-6 py-2 border border-gray-300 rounded-md text-gray-700 hover:bg-gray-50 transition font-medium" type="reset">Limpar</button>
+        </>
+    )
+}
